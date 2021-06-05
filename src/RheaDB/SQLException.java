a2cf6a91f29b32bac0df9e0ac1543d4e9f8794c0
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-06-05 07:42:28 +0200

    Added SQLException.

---
+++ src/RheaDB/SQLException.java
@@ -0,0 +1,12 @@
+package RheaDB;
+
+public class SQLException extends Exception {
+
+    public SQLException() {
+        super();
+    }
+
+    public SQLException(String message) {
+        super(message);
+    }
+}
