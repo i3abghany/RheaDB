f9ae514542b6b8ef2276213c2104f71cbd568419
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-06-17 17:16:12 +0200

    Predicate: Fixed comparisons.

--- src/Predicate/GreaterThanPredicate.java
+++ src/Predicate/GreaterThanPredicate.java
@@ -9,9 +9,9 @@ public class GreaterThanPredicate extends Predicate {
     @Override
     public boolean doesSatisfy(Object comp) {
         return switch (this.attribute.getType()) {
-            case INT -> ((Integer) value).compareTo((Integer) comp) > 0;
-            case STRING -> ((String) value).compareTo((String) comp) > 0;
-            case FLOAT -> ((Float) value).compareTo((Float) comp) > 0;
+            case INT -> ((Integer) value).compareTo((Integer) comp) < 0;
+            case STRING -> ((String) value).compareTo((String) comp) < 0;
+            case FLOAT -> ((Float) value).compareTo((Float) comp) < 0;
         };
     }
 
