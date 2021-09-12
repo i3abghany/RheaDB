f54fcf0a6b50efdd8365433c0b709fe128361790
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-09-12 23:46:28 +0200

    QP: Make base class for all statement parsers.

--- src/QueryProcessor/SelectParser.java
+++ src/QueryProcessor/SelectParser.java
@@ -1,21 +1,22 @@
 package QueryProcessor;
 
-import Predicate.*;
 import Predicate.Predicate;
 import RheaDB.DBError;
 
 import java.util.Arrays;
-import java.util.Locale;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class SelectParser {
-    private final String regex;
-    private final String line;
+public class SelectParser extends StatementParser {
+
+    private final int ATTRIBUTES_GROUP = 1;
+    private final int TABLENAME_GROUP = 2;
+    private final int PREDICATES_GROUP = 3;
+
     public SelectParser(String line) {
-        this.line  = line;
+        super(line);
         this.regex = "select\\s+(.*?)\\s*from\\s+(.*?)\\s*(where\\s(.*?)\\s*)?;";
     }
 
@@ -27,18 +28,19 @@ public class SelectParser {
             throw new DBError("Error parsing the statement.");
         }
 
-        Vector<String> attributeNames = Arrays.stream(matcher.group(1).split(
-                ","))
-                .map(String::trim)
-                .collect(Collectors.toCollection(Vector::new));
-        String tableName = matcher.group(2);
-        boolean usePredicates = matcher.group(3) != null;
+        Vector<String> attributeNames =
+                Arrays.stream(matcher.group(ATTRIBUTES_GROUP).split(","))
+                        .map(String::trim)
+                        .collect(Collectors.toCollection(Vector::new));
+        String tableName = matcher.group(TABLENAME_GROUP);
+        boolean usePredicates = matcher.group(PREDICATES_GROUP) != null;
 
         if (!usePredicates) {
-            return new DMLStatement.SelectStatement(tableName, attributeNames, new Vector<>());
+            return new DMLStatement.SelectStatement(tableName, attributeNames
+                    , new Vector<>());
         }
 
-        String[] predicateStrings = matcher.group(3).split(",");
+        String[] predicateStrings = matcher.group(PREDICATES_GROUP).split(",");
         predicateStrings[0] = predicateStrings[0].split(" ", 2)[1];
 
         Vector<Predicate> predicates = new Vector<>();
@@ -54,32 +56,7 @@ public class SelectParser {
                     tokens.elementAt(2).getValue()));
         }
 
-        return new DMLStatement.SelectStatement(tableName, attributeNames, predicates);
-    }
-
-    // TODO: This may be removed once we can inherit from Parser.
-    private Parser.OperatorKind getOperatorKind(String op) {
-        return switch (op) {
-            case "=" -> Parser.OperatorKind.EqualsOperator;
-            case "!=" -> Parser.OperatorKind.NotEqualsOperator;
-            case ">" -> Parser.OperatorKind.GreaterOperator;
-            case ">=" -> Parser.OperatorKind.GreaterEqualsOperator;
-            case "<" -> Parser.OperatorKind.LessOperator;
-            case "<=" -> Parser.OperatorKind.LessEqualsOperator;
-            default -> Parser.OperatorKind.UnsupportedOperator;
-        };
-    }
-
-    // TODO: This may be removed once we can inherit from Parser.
-    private Predicate parsePredicate(String attributeName, String operator, Object value) {
-        return switch (getOperatorKind(operator)) {
-            case EqualsOperator -> new EqualsPredicate(attributeName, value);
-            case NotEqualsOperator -> new NotEqualsPredicate(attributeName, value);
-            case GreaterOperator -> new GreaterThanPredicate(attributeName, value);
-            case GreaterEqualsOperator -> new GreaterThanEqualPredicate(attributeName, value);
-            case LessOperator -> new LessThanPredicate(attributeName, value);
-            case LessEqualsOperator -> new LessThanEqualPredicate(attributeName, value);
-            default -> null;
-        };
+        return new DMLStatement.SelectStatement(tableName, attributeNames,
+                predicates);
     }
 }
---
+++ src/QueryProcessor/StatementParser.java
@@ -0,0 +1,40 @@
+package QueryProcessor;
+
+import Predicate.*;
+import Predicate.Predicate;
+import RheaDB.DBError;
+
+public abstract class StatementParser {
+    protected String line;
+    protected String regex;
+
+    public StatementParser(String line) {
+        this.line = line;
+    }
+
+    public abstract SQLStatement parse() throws DBError;
+
+    protected Parser.OperatorKind getOperatorKind(String op) {
+        return switch (op) {
+            case "=" -> Parser.OperatorKind.EqualsOperator;
+            case "!=" -> Parser.OperatorKind.NotEqualsOperator;
+            case ">" -> Parser.OperatorKind.GreaterOperator;
+            case ">=" -> Parser.OperatorKind.GreaterEqualsOperator;
+            case "<" -> Parser.OperatorKind.LessOperator;
+            case "<=" -> Parser.OperatorKind.LessEqualsOperator;
+            default -> Parser.OperatorKind.UnsupportedOperator;
+        };
+    }
+
+    protected Predicate parsePredicate(String attributeName, String operator, Object value) {
+        return switch (getOperatorKind(operator)) {
+            case EqualsOperator -> new EqualsPredicate(attributeName, value);
+            case NotEqualsOperator -> new NotEqualsPredicate(attributeName, value);
+            case GreaterOperator -> new GreaterThanPredicate(attributeName, value);
+            case GreaterEqualsOperator -> new GreaterThanEqualPredicate(attributeName, value);
+            case LessOperator -> new LessThanPredicate(attributeName, value);
+            case LessEqualsOperator -> new LessThanEqualPredicate(attributeName, value);
+            default -> null;
+        };
+    }
+}
