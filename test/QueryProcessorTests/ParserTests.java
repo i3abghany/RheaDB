6007fb4614cd28745d6be7e4375d82cc6976be19
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2021-10-13 23:15:54 +0200

    Tests: Add a compound predicate parsing test.

--- test/QueryProcessorTests/ParserTests.java
+++ test/QueryProcessorTests/ParserTests.java
@@ -5,7 +5,9 @@ import QueryParser.DDLStatement.*;
 import QueryParser.DMLStatement.*;
 import QueryParser.InternalStatement.*;
 import QueryParser.Parser;
+import QueryParser.PredicateParser.*;
 import QueryParser.SQLStatement;
+import QueryParser.TokenKind;
 import RheaDB.Attribute;
 import RheaDB.AttributeType;
 import RheaDB.DBError;
@@ -356,5 +358,56 @@ public class ParserTests {
         }
 
         Assertions.assertTrue(updateStatement.getWherePredicates().isEmpty());
+    }
+
+    @Test
+    void parseCompoundPredicate() throws Exception {
+        String predicate = "(a > 10 && b <= 12) || a != 2";
+        PredicateAST ast = new PredicateParser(predicate).parse();
+        Assertions.assertTrue(ast.root() instanceof BinaryLogicalExpression);
+
+        var left = ((BinaryLogicalExpression) ast.root()).getLhs();
+        var operator = ((BinaryLogicalExpression) ast.root()).getOperatorToken();
+        var right = ((BinaryLogicalExpression) ast.root()).getRhs();
+
+        Assertions.assertTrue(left instanceof ParenthesizedExpression);
+        Assertions.assertEquals(operator.getKind(), TokenKind.BarBarToken);
+        Assertions.assertTrue(right instanceof BinaryLogicalExpression);
+
+        var enclosedExpression = ((ParenthesizedExpression) left).getExpression();
+        Assertions.assertTrue(enclosedExpression instanceof BinaryLogicalExpression);
+
+        left = ((BinaryLogicalExpression) enclosedExpression).getLhs();
+        operator = ((BinaryLogicalExpression) enclosedExpression).getOperatorToken();
+        right = ((BinaryLogicalExpression) enclosedExpression).getRhs();
+
+        Assertions.assertTrue(left instanceof BinaryLogicalExpression);
+        Assertions.assertEquals(operator.getKind(), TokenKind.AmpersandAmpersandToken);
+        Assertions.assertTrue(right instanceof BinaryLogicalExpression);
+
+        var aGreaterThan10 = (BinaryLogicalExpression) left;
+        Assertions.assertNotNull(aGreaterThan10);
+        var aIdentifier = aGreaterThan10.getLhs();
+        var greaterOperator = aGreaterThan10.getOperatorToken();
+        var tenLiteral = aGreaterThan10.getRhs();
+
+        Assertions.assertTrue(aIdentifier instanceof IdentifierExpression);
+        Assertions.assertEquals(((IdentifierExpression) aIdentifier).getIdentifierToken().getValue(), "a");
+        Assertions.assertEquals(greaterOperator.getKind(), TokenKind.GreaterToken);
+        Assertions.assertTrue(tenLiteral instanceof LiteralExpression);
+        Assertions.assertEquals(((LiteralExpression) tenLiteral).getValueToken().getValue(), 10);
+
+        var bLessEqualsThan12 = (BinaryLogicalExpression) right;
+        Assertions.assertNotNull(bLessEqualsThan12);
+
+        var bIdentifier = bLessEqualsThan12.getLhs();
+        var lessEquals = bLessEqualsThan12.getOperatorToken();
+        var twelveLiteral = bLessEqualsThan12.getRhs();
+
+        Assertions.assertTrue(bIdentifier instanceof IdentifierExpression);
+        Assertions.assertEquals(((IdentifierExpression) bIdentifier).getIdentifierToken().getValue(), "b");
+        Assertions.assertEquals(lessEquals.getKind(), TokenKind.LessEqualsToken);
+        Assertions.assertTrue(twelveLiteral instanceof LiteralExpression);
+        Assertions.assertEquals(((LiteralExpression) twelveLiteral).getValueToken().getValue(), 12);
     }
 }
