3eb3039e052b7a76bcc9d6d1948157b9108a231e
mam47 <mam47@sfu.ca>
2026-05-14 22:05:43 -0700

    B+ tree: search leaf keys by binary search

--- src/BPlusTree/LeafNode.java
+++ src/BPlusTree/LeafNode.java
@@ -8,9 +8,9 @@ public class LeafNode<K extends Comparable<K>, V> extends Node<K> {
     private final int maxLists;
     private final int minLists;
 
+    @SuppressWarnings("unchecked")
     public LeafNode(int order) {
         super(order, null);
-        this.order = order;
         this.maxLists = order - 1;
         this.minLists = (int) Math.ceil(this.order / 2.0) - 1;
         this.valueLists = new ValueList[this.order];
@@ -26,57 +26,47 @@ public class LeafNode<K extends Comparable<K>, V> extends Node<K> {
     }
 
     public void insert(K key, V val) {
-        ValueList<K, V> existentList = exists(key);
-        if (existentList != null)
-            insertDuplicate(existentList, val);
-        else if (this.numberOfLists < this.order) {
-            this.valueLists[this.numberOfLists] = new ValueList<>(key, val);
-            this.numberOfLists++;
-            sortLists();
+        int idx = findListIndex(key);
+        if (idx >= 0) {
+            this.valueLists[idx].add(val);
+            return;
         }
+
+        insertListAt(-(idx + 1), new ValueList<>(key, val));
     }
 
     public void insert(ValueList<K, V> list) {
-        if (this.numberOfLists < this.order) {
-            this.valueLists[this.numberOfLists] = list;
-            this.numberOfLists++;
-            sortLists();
+        int idx = findListIndex(list.getKey());
+        if (idx >= 0) {
+            this.valueLists[idx].addAll(list);
+            return;
         }
-    }
 
-    private void insertDuplicate(ValueList<K, V> valueList, V val) {
-        valueList.add(val);
+        insertListAt(-(idx + 1), list);
     }
 
     public ValueList<K, V> exists(K key) {
-        for (int i = 0; i < numberOfLists; i++) {
-            if (this.valueLists[i].getKey().equals(key))
-                return this.valueLists[i];
-        }
-        return null;
+        int idx = findListIndex(key);
+        return idx >= 0 ? this.valueLists[idx] : null;
     }
 
-    // DELETES A WHOLE LIST...
     public boolean deleteByIndex(int idx) {
-        if (idx < 0 || idx > this.numberOfLists - 1)
+        if (idx < 0 || idx >= this.numberOfLists) {
             return false;
-        if (this.numberOfLists - (idx + 1) >= 0)
-            System.arraycopy(this.valueLists, idx + 1, this.valueLists, idx, this.numberOfLists - (idx + 1));
+        }
+
+        int movedLists = this.numberOfLists - idx - 1;
+        if (movedLists > 0) {
+            System.arraycopy(this.valueLists, idx + 1, this.valueLists, idx, movedLists);
+        }
+
         this.valueLists[this.numberOfLists - 1] = null;
         this.numberOfLists--;
-        Arrays.sort(valueLists, 0, numberOfLists);
-
         return true;
     }
 
     public boolean deleteByKey(K key) {
-        int idx = Arrays.binarySearch(
-                this.valueLists,
-                0,
-                this.numberOfLists,
-                new ValueList<K, V>(key, null)
-        );
-
+        int idx = findListIndex(key);
         return deleteByIndex(idx);
     }
 
@@ -87,17 +77,20 @@ public class LeafNode<K extends Comparable<K>, V> extends Node<K> {
     public void setLists(ValueList<K, V>[] lists, int n) {
         this.valueLists = lists;
         this.numberOfLists = n;
+        Arrays.fill(this.valueLists, this.numberOfLists, this.valueLists.length, null);
     }
 
+    @SuppressWarnings("unchecked")
     public ValueList<K, V>[] splitLists(int mid) {
         ValueList<K, V>[] rightList = new ValueList[this.order];
+        int rightListCount = this.numberOfLists - mid;
 
-        for (int i = mid; i < this.order; i++) {
-            rightList[i - mid] = this.valueLists[i];
-            this.valueLists[i] = null;
-            this.numberOfLists--;
+        if (rightListCount > 0) {
+            System.arraycopy(this.valueLists, mid, rightList, 0, rightListCount);
+            Arrays.fill(this.valueLists, mid, this.numberOfLists, null);
         }
 
+        this.numberOfLists = mid;
         return rightList;
     }
 
@@ -131,28 +124,61 @@ public class LeafNode<K extends Comparable<K>, V> extends Node<K> {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void merge(Node<K> lf) {
         LeafNode<K, V> leaf = (LeafNode<K, V>) lf;
         for (int i = 0; i < leaf.getNumberOfLists(); i++) {
-            this.valueLists[this.numberOfLists] = leaf.getLists()[i];
-            this.numberOfLists++;
+            insert(leaf.getLists()[i]);
         }
-        this.sortLists();
     }
 
-    private void sortLists() {
-        Arrays.sort(this.valueLists, (o1, o2) -> {
-            if (o1 == null && o2 == null) {
-                return 0;
+    public int firstGreaterOrEqualIndex(K key) {
+        int idx = findListIndex(key);
+        return idx >= 0 ? idx : -(idx + 1);
+    }
+
+    public int firstGreaterThanIndex(K key) {
+        int idx = firstGreaterOrEqualIndex(key);
+        while (idx < numberOfLists && valueLists[idx].getKey().compareTo(key) == 0) {
+            idx++;
+        }
+        return idx;
+    }
+
+    private void insertListAt(int idx, ValueList<K, V> list) {
+        if (this.numberOfLists >= this.valueLists.length) {
+            throw new IllegalStateException("Cannot insert value list into a full leaf.");
+        }
+        if (idx < 0 || idx > this.numberOfLists) {
+            throw new IndexOutOfBoundsException("Invalid list index: " + idx);
+        }
+
+        int movedLists = this.numberOfLists - idx;
+        if (movedLists > 0) {
+            System.arraycopy(this.valueLists, idx, this.valueLists, idx + 1, movedLists);
+        }
+
+        this.valueLists[idx] = list;
+        this.numberOfLists++;
+    }
+
+    private int findListIndex(K key) {
+        int low = 0;
+        int high = numberOfLists - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int comparison = valueLists[mid].getKey().compareTo(key);
+
+            if (comparison < 0) {
+                low = mid + 1;
+            } else if (comparison > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
             }
-            if (o1 == null) {
-                return 1;
-            }
-            if (o2 == null) {
-                return -1;
-            }
-            return o1.compareTo(o2);
-        });
+        }
+
+        return -(low + 1);
     }
 }
-
